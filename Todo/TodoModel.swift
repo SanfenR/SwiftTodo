ee5b177ac43611a9d3ab46006bebd9ca5826fdb2
sanfenr <sanfenruxi1@163.com>
2017-09-07 18:36:53 +0800

    创建model

--- Todo/TodoModel.swift
+++ Todo/TodoModel.swift
@@ -6,5 +6,15 @@
 import UIKit
 
 class TodoModel : NSObject {
- 
+    var id: String
+    var image: String
+    var title: String
+    var date: Date
+
+    init(id: String, image: String, title: String, date: Date) {
+        self.id = id
+        self.image = image
+        self.title = title
+        self.date = date
+    }
 }
